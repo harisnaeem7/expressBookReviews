b683c4df76b3a955c70205d1bf22f96d052255c4
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-11 12:42:43 -0700

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,8 +32,19 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let author = req.params.author;
+  const keys = Object.entries(books);
+
+  const authorSpecificBooks = keys.filter(([key, book]) => {
+    return book.author.toLocaleLowerCase() === author.toLocaleLowerCase();
+  });
+
+  if (authorSpecificBooks.length > 0) {
+    const data = JSON.stringify(authorSpecificBooks);
+    return res.status(300).json(data);
+  } else {
+    return res.status(300).json({ message: "Author Not Found" });
+  }
 });
 
 // Get all books based on title
