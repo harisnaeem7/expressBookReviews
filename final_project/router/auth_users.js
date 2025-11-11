7c8c526ecd5dd4875b979f30cd1ad9f86b308772
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-11 14:48:12 -0700

    Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,66 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  const existingUser = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  if (existingUser.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (!username || !password) {
+    return res
+      .status(300)
+      .json({ message: "Please enter valid username and password" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "fingerprint_customer",
+      {
+        expiresIn: 60 * 60,
+      }
+    );
+
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+
+    return res.status(300).json({ message: "Logged in Successfully!" });
+  } else {
+    return res.status(300).json({ message: "User not found" });
+  }
+
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
